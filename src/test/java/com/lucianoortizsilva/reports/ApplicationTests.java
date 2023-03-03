31d16b9dbcb4347efbcd8e089f01d221da30dba7
Luciano Ortiz <lucianoortizsilva@gmail.com>
2023-03-03 15:27:06 -0300

    Add projeto inicial com Sprong Boot

---
+++ src/test/java/com/lucianoortizsilva/reports/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lucianoortizsilva.reports;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
