618ac618943a17396e4c957cc9b46cd0979cc1bf
Luciano Ortiz <lucianoortizsilva@gmail.com>
2023-03-04 00:47:03 -0300

    Criando tabelas + inserindo dados na base MySQL

--- src/main/java/com/lucianoortizsilva/reports/Application.java
+++ src/main/java/com/lucianoortizsilva/reports/Application.java
@@ -2,7 +2,9 @@ package com.lucianoortizsilva.reports;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
 @SpringBootApplication
 public class Application {
 
